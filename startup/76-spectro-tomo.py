Removed all scaling factors applied to zpssx/z stages, as ppmac has been recalibrated to show the correct positions.

startup/76-spectro-tomo.py
@@ -252,8 +252,8 @@ def zp_tomo_2d_scan(angle,dets_,x_start,x_end,x_num,y_start,y_end,y_num,exp):
     # x_scale_factor = 0.9542
     # z_scale_factor = 1.0309
 
-    x_scale_factor = abs(motor_table['zpssx'][1] * 1.e4)
-    z_scale_factor = abs(motor_table['zpssz'][1] * 1.e4)
+    x_scale_factor = 1
+    z_scale_factor = 1
 
     if np.abs(angle) < 44.99:
                 